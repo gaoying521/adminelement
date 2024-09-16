069571a0632c50103caa667c0b968dd3bd14f837
yuanshun <1422672890@qq.com>
2024-09-16 13:44:17 +0800

    qweffew

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">Afefewfewbout</router-link>
+    <router-link to="/about">Afefregegrgrrgeewfewbout</router-link>
   </nav>
   <router-view />
 </template>
