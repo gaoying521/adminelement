9af2b51ef2d9a2252b9d51b094187cc692373cc9
yuanshun <1422672890@qq.com>
2024-09-16 13:18:04 +0800

    qw3qdrw

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an a的深层次的谁对谁错bo色的风十多万群多群格是各色ut page</h1>
+    <h1>
+      This is an a的深层次的谁对谁错bo色grrrrb的风十多万群多群格是各色ut page
+    </h1>
   </div>
 </template>
